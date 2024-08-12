f47c49ebdc9bec512d24d1561e108e5ff871157b
koribillRamudu <sekharramudu9054@msitprogram.net>
2024-08-12 23:30:59 +0530

    peek element in array

---
+++ peekelement.py
@@ -0,0 +1,10 @@
+def peakElement(arr, n):
+    m=0
+    v=0
+    for i in range(n):
+        if arr[i]>=m:
+            m=arr[i]
+            v=i
+        return v
+        # n=max(arr)
+        # return arr.index(n)
